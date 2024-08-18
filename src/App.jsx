ed3f3afa25f6c8c2fb951c40983c192e79483c69
nohasalim <noha.fcs@gmail.com>
2024-08-18 23:35:28 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [formData, setformData] = useState([
+    {
+      firstName: "",
+      lastName: "",
+      email: "",
+      gender: "",
+      massege: "",
+      ruleAccepted: false,
+    },
+  ]);
+  function handleOnchange(event) {
+    var keyName = event.target.name;
+    var KeyValue = event.target.value;
+    var keyType = event.target.type;
+    if (keyType == "checkbox") {
+      KeyValue = event.target.checked;
+    }
+    setformData({ ...formData, [keyName]: KeyValue });
+  }
+  function handelFormOnSubmit(event) {
+    console.log(formData);
+    event.preventDefault();
+  }
+  return (
+    <main>
+      <form onSubmit={handelFormOnSubmit}>
+        <label htmlFor="firstName">First Name *</label>
+        <input
+          id="firstName"
+          name="firstName"
+          onChange={handleOnchange}
+          value={formData.firstName}
+          type="text"
+        ></input>
+        <label htmlFor="lastName">Last Name *</label>
+        <input
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleOnchange}
+          type="text"
+        ></input>
+        <label htmlFor="emailAdress">Email Adress *</label>
+        <input
+          name="email"
+          value={formData.email}
+          onChange={handleOnchange}
+          type="email"
+        ></input>
+        <label htmlFor="gender">Gender *</label>
+        <div>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            onChange={handleOnchange}
+          ></input>
+          <label htmlFor="male"> Male</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            onChange={handleOnchange}
+          ></input>
+          <label htmlFor="female">Female</label>
+        </div>
+
+        <label htmlFor="massege">Massege *</label>
+        <textarea
+          name="massege"
+          value={formData.massege}
+          onChange={handleOnchange}
+        ></textarea>
+        <div>
+          <input
+            name="ruleAccepted"
+            type="checkbox"
+            onChange={handleOnchange}
+            checked={formData.ruleAccepted}
+          ></input>
+          <label>I consent to being contacted by the team .</label>
+        </div>
+
+        <button type="submit" disabled={formData.ruleAccepted ? false : true}>
+          Submit
+        </button>
+      </form>
+    </main>
+  );
+}
+
+export default App;
